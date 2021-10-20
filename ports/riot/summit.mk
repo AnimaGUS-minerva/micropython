3e378fe812b475a6fbb03d89ab4a6e30e8a9c871
j-devel <j@w3reality.com>
2021-10-20 14:02:53 +0700

    ports/riot: Build support with the latest RIOT-OS
    Follow the latest instructions on handling external modules according to
    https://doc.riot-os.org/creating-modules.html

--- ports/riot/summit.mk
+++ ports/riot/summit.mk
@@ -3,13 +3,12 @@ all: build
 GENHDR_DIR := ../../genhdr
 
 RIOT:
-	git -c advice.detachedHead=false \
-		clone --branch 2021.04 https://github.com/RIOT-OS/RIOT
+	@if [ ! -e RIOT ]; then \
+		git clone https://github.com/RIOT-OS/RIOT; fi
 
 build: RIOT
 	mkdir -p $(GENHDR_DIR) && touch $(GENHDR_DIR)/qstrdefs.generated.h
 	RIOTBASE=$(CURDIR)/RIOT make -C main
 
 clean:
-	rm main/bin \
-		-rf $(GENHDR_DIR) ../../frozen.c \
+	rm -rf main/bin $(GENHDR_DIR) ../../frozen.c
